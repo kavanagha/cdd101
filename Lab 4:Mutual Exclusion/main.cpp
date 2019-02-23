24bef516f8b441e197a85004771a77bd8039fe4a
kavanagha <ailish.j.kavanagh@gmail.com>
2019-02-23 23:13:39 +0000

    Lab 4: Mutual Exclusion complete

--- Lab 4:Mutual Exclusion/main.cpp
+++ Lab 4:Mutual Exclusion/main.cpp
@@ -14,21 +14,31 @@ int sharedVariable=0;
    Uses C++11 features such as mutex and condition variables to implement an example of a rendezvous for threads
 
 */
+
+/*
+*Initialising the semaphore to be locked
+*Each thread will update shared variable sequentially using wait()
+*They will unlock it once they are done with Signal()
+* Forces the threads to enter the mutex lock one at a time
+*/
 /*! displays a message that is split in to 2 sections to show how a rendezvous works*/
 void updateTask(std::shared_ptr<Semaphore> firstSem, int numUpdates){
 
- 
+  firstSem->Wait();
+
   for(int i=0;i<numUpdates;i++){
     //UPDATE SHARED VARIABLE HERE!
     sharedVariable++;
   }
+
+  firstSem->Signal();
 
 }
 
 
 int main(void){
   std::vector<std::thread> vt(num_threads);
-  std::shared_ptr<Semaphore> aSemaphore( new Semaphore);
+  std::shared_ptr<Semaphore> aSemaphore( new Semaphore(1));
   /**< Launch the threads  */
   int i=0;
   for(std::thread& t: vt){
